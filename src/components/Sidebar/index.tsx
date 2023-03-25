c3eca3c57ecfeab4c05bf3feb291ff9bdd495fba
devstackpro <89517315+devstackpro@users.noreply.github.com>
2023-03-25 18:33:26 -0300

    Criando o elemento de navegação

--- src/components/Sidebar/index.tsx
+++ src/components/Sidebar/index.tsx
@@ -1,5 +1,21 @@
 import { Container } from "./styles"
+import menuImg from '../../assets/menu.svg'
+import { ReactComponent as CervejaIcon } from '../../assets/cerveja.svg'
 
 export function Sidebar() {
-  return <Container>Sidebar</Container>
+  return <Container>
+    <button type='button'>
+      <img src={menuImg} alt='Abrie e fechar o menu' />
+    </button>
+    <nav>
+      <ul>
+        <li>
+          <a href='#'>
+            <CervejaIcon />
+            <span>Cervejas</span>
+          </a>
+        </li>
+      </ul>
+    </nav>
+  </Container>
 }
