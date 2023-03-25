62a7a4cc18c6088464271818d242d5a3fccf967f
devstackpro <89517315+devstackpro@users.noreply.github.com>
2023-03-25 17:16:34 -0300

    Criando a estrutura inicial da page Main

--- src/pages/Main/styles.ts
+++ src/pages/Main/styles.ts
@@ -1,6 +1,8 @@
 import styled from 'styled-components'
 
-export const Container = styled.div`
-  text-align: center;
-  margin-top: 2rem;
+export const Container = styled.main`
+  width: 100%; //lagura da página
+  min-height: 100vh; //altura da página
+
+  display: flex; //para alinhar as coisas na laterarl
 `
