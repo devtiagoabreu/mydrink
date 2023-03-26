ae9f92eacb78f50b63e57ce94d9e6bf850c58793
devstackpro <89517315+devstackpro@users.noreply.github.com>
2023-03-26 07:35:19 -0300

    Acrescetando os demais icones

--- src/components/Sidebar/index.tsx
+++ src/components/Sidebar/index.tsx
@@ -1,6 +1,11 @@
 import { Container } from "./styles"
 import menuImg from '../../assets/menu.svg'
 import { ReactComponent as CervejaIcon } from '../../assets/cerveja.svg'
+import { ReactComponent as WhiskeyIcon } from '../../assets/whiskey.svg'
+import { ReactComponent as VinhoIcon } from '../../assets/vinho.svg'
+import { ReactComponent as EnergyDrinkIcon } from '../../assets/energy-drink.svg'
+import { ReactComponent as RefrigeranteIcon } from '../../assets/refrigerante.svg'
+
 
 export function Sidebar() {
   return <Container>
@@ -13,6 +18,30 @@ export function Sidebar() {
           <a href='#' >
             <CervejaIcon />
             <span>Cervejas</span>
+          </a>
+        </li>
+        <li>
+          <a href='#' >
+            <WhiskeyIcon />
+            <span>Destilados</span>
+          </a>
+        </li>
+        <li>
+          <a href='#' >
+            <VinhoIcon />
+            <span>Vinhos</span>
+          </a>
+        </li>
+        <li>
+          <a href='#' >
+            <EnergyDrinkIcon />
+            <span>Energéticos</span>
+          </a>
+        </li>
+        <li>
+          <a href='#' >
+            <RefrigeranteIcon />
+            <span>Refrigerantes</span>
           </a>
         </li>
       </ul>
