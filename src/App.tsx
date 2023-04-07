764030d5f4d65ad9d1ef7eca9851ea526cf7e087
Tiago Abreu <devtiagoabreu@gmail.com>
2023-04-07 07:00:13 -0300

    inserindo logo e iniciando páginas do main

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,5 @@
 import { BrowserRouter } from 'react-router-dom'
-
 import { AppRoutes } from './routes'
-
 import { Theme } from './styles/Theme'
 import { GlobalStyle } from './styles/global'
 import { Normalize } from 'styled-normalize'
--- src/routes.tsx
+++ src/routes.tsx
@@ -1,11 +1,13 @@
 import { Routes, Route } from 'react-router-dom'
 
 import MainPage from './pages/Main'
+import BurgersPage from './pages/Main/Burgers'
 
 export function AppRoutes() {
   return (
     <Routes>
       <Route path='/' element={<MainPage />} />
+      <Route path='/' element={<BurgersPage />} />
     </Routes>
   )
 }
