5293b5e57ce9b5e9899f41a5b8cb596a7bbb8191
Tiago Abreu <devtiagoabreu@gmail.com>
2024-04-07 16:57:58 -0300

    array de burgers

--- src/pages/Main/Burgers/index.tsx
+++ src/pages/Main/Burgers/index.tsx
@@ -3,6 +3,27 @@ import { SnackTitle } from '../../../components/SnackTitle'
 
 
 export default function Burgers() {
+  const data = [
+    {
+      id: 1,
+      snack: 'burger',
+      name: 'Mega',
+      description: 'O artesanal tamanho família, recheado com três carnes suculentas, queijo e bacon.',
+      price: 25.5,
+      image: 'https://i.imgur.com/upjIUnG.jpg',
+    },
+    {
+      id: 2,
+      snack: 'burger',
+      name: 'Extra Bacon',
+      description: 'Criado para os amantes de bacon, possui em todas as suas camadas bacon bem assado, e ainda quejo e carne.',
+      price: 23.5,
+      image: 'https://i.imgur.com/B4J04AJ.jpg',
+    },
+]
+
+  console.log('data', data);
+
   return (
     <>
       <Head title='Hamburgueres' description='Nossos melhores burguers' />
