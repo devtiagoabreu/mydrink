3115950a14d46d7643bd79ea5bd34245b9296a57
Tiago Abreu <devtiagoabreu@gmail.com>
2024-04-08 01:55:01 -0300

    .

--- src/pages/Main/Land/index.tsx
+++ src/pages/Main/Land/index.tsx
@@ -7,7 +7,7 @@ export default function Land() {
     {
       id: 2,
       pannel: 'land',
-      name: 'Quarta - Dia do Burguer',
+      name: 'Qua - Dia do Burguer',
       description: 'Quarta é Dia do Burguer na Sancta Cervejaria! Venha saborear os melhores hambúrgueres da cidade.',
       important: '',
       image: 'https://files.menudino.com/cardapios/8164/762.jpg',
@@ -15,15 +15,15 @@ export default function Land() {
     {
       id: 3,
       pannel: 'land',
-      name: 'Quinta - karaokê',
-      description: 'Igor Guimarães.',
-      important: 'stand-up comedy!',
-      image: 'https://ohoje.com/public/imagens/fotos/amp/2022/11/13-MATERIA-CAPA-22-1024x576.jpg',
+      name: 'Qui - karaokê',
+      description: 'Outro pilar que sustenta a Sancta é a arte, através principalmente da música ao vivo de ótima qualidade.',
+      important: '',
+      image: 'https://liberal.com.br/wp-content/uploads/2022/07/Espa%C3%A7o-re%C3%BAne-boa-cerveja-de-produ%C3%A7%C3%A3o-pr%C3%B3pria-gastronomia-um-ambiente-descontra%C3%ADdo-m%C3%BAsica-e-arte__Divulga%C3%A7%C3%A3o.jpg',
     },
     {
       id: 4,
       pannel: 'land',
-      name: 'Sexta - Banda Fly End',
+      name: 'Sex - Banda Fly End',
       description: 'Banda de pop-rock, nacional e internacional com influencias do rock dos anos 80 e 90',
       important: '',
       image: 'https://mydrink.com.br/images/bandaflyend.jpg',
@@ -31,7 +31,7 @@ export default function Land() {
     {
       id: 5,
       pannel: 'land',
-      name: 'Sábado - Juarez Angelini',
+      name: 'Sáb - Juarez Angelini',
       description: 'Estilo pop rock nacional e internacional: U2, Oasis, Legião Urbana, Lulu Santos, sucessos dos anos 80 e 90.',
       important: '',
       image: 'https://mydrink.com.br/images/juarezangelini.jpg',
