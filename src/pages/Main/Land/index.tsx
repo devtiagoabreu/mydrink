1b04f4db5e79b2c2c087c5987c87758e2005e05b
Tiago Abreu <devtiagoabreu@gmail.com>
2024-04-08 01:48:54 -0300

    .

--- src/pages/Main/Land/index.tsx
+++ src/pages/Main/Land/index.tsx
@@ -5,20 +5,12 @@ import { Pannel } from '../../../components/Pannel'
 export default function Land() {
   const data = [
     {
-      id: 1,
-      pannel: 'land',
-      name: 'Programação da Semana',
-      description: '• Quarta - Dia do Burguer • Quinta - karaokê • Sexta - Banda Fly End • Sábado - Juarez Angelini •',
-      important: 'Resumão',
-      image: 'https://liberal.com.br/wp-content/uploads/2022/07/Espa%C3%A7o-re%C3%BAne-boa-cerveja-de-produ%C3%A7%C3%A3o-pr%C3%B3pria-gastronomia-um-ambiente-descontra%C3%ADdo-m%C3%BAsica-e-arte__Divulga%C3%A7%C3%A3o.jpg',
-    },
-    {
       id: 2,
       pannel: 'land',
       name: 'Quarta - Dia do Burguer',
-      description: 'Igor Guimarães.',
-      important: 'stand-up comedy!',
-      image: 'https://ohoje.com/public/imagens/fotos/amp/2022/11/13-MATERIA-CAPA-22-1024x576.jpg',
+      description: 'Quarta é Dia do Burguer na Sancta Cervejaria! Venha saborear os melhores hambúrgueres da cidade.',
+      important: '',
+      image: 'https://files.menudino.com/cardapios/8164/762.jpg',
     },
     {
       id: 3,
@@ -32,16 +24,16 @@ export default function Land() {
       id: 4,
       pannel: 'land',
       name: 'Sexta - Banda Fly End',
-      description: 'Igor Guimarães.',
-      important: 'stand-up comedy!',
-      image: 'https://ohoje.com/public/imagens/fotos/amp/2022/11/13-MATERIA-CAPA-22-1024x576.jpg',
+      description: 'Banda de pop-rock, nacional e internacional com influencias do rock dos anos 80 e 90',
+      important: '',
+      image: 'https://mydrink.com.br/images/bandaflyend.jpg',
     },
     {
       id: 5,
       pannel: 'land',
       name: 'Sábado - Juarez Angelini',
-      description: 'Músico há mais de dez anos. Focado no estilo pop rock nacional e internacional como referência artistas como U2, Oasis, Legião Urbana, Lulu Santos, grandes sucessos da anos 80 e 90.',
-      important: 'Acesse uma palhinha.',
+      description: 'Estilo pop rock nacional e internacional: U2, Oasis, Legião Urbana, Lulu Santos, sucessos dos anos 80 e 90.',
+      important: '',
       image: 'https://mydrink.com.br/images/juarezangelini.jpg',
     },
 
