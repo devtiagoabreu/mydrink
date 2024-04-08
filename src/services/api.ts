036fcbbb9751be22a39c66b4b6fa7d13fcf9c76b
Tiago Abreu <devtiagoabreu@gmail.com>
2024-04-08 20:20:16 -0300

    Criando as funções de APIs iniciais - axios

--- src/services/api.ts
+++ src/services/api.ts
@@ -4,4 +4,12 @@ const api = axios.create({
   baseURL: process.env.REACT_APP_API_BASE_URL,
 })
 
+export const getLand = () => api.get('/land')
+export const getCervejas = () => api.get('/Cervejas')
+export const getBurgers = () => api.get('/burgers')
+export const getPorcoes = () => api.get('/Porcoes')
+export const getSAlcool = () => api.get('/SAlcool')
+
+
+
 export default api
